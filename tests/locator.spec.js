00dad7f42967c9e3e628e08407fc7f03312746ad
KhansaRT <khansarafifah89@gmail.com>
2025-02-09 20:29:07 +0700

    implementasi RestAPI

--- tests/locator.spec.js
+++ tests/locator.spec.js
@@ -1,7 +1,6 @@
 // @ts-check
-import { test, expect } from '@playwright/test';
-import CheckoutActions from './pom/objectActions/checkoutActions';
-import checkoutActions from './pom/objectActions/checkoutActions';
+import { test, expect, request } from '@playwright/test';
+const { default: checkoutActions } = require('../tests/pom/objectActions/checkoutActions');
 const { default: loginActions } = require('../tests/pom/objectActions/loginActions');
 
 test('Login', async ({ page }) =>{
@@ -14,4 +13,28 @@ test('Checkout', async({ page }) =>{
   const objCheckout = new checkoutActions(page);
   await objCheckout.goto();
   await objCheckout.Checkout();
+});
+
+test('contoh get', async ({ page }) => {
+    const apiContext = await request.newContext();
+    const res = await apiContext.get('https://reqres.in/api/users?page=2');
+    expect(res.status()).toBe(200);
+    const respondJSON = await res.json();
+    expect(respondJSON.page).toBe(2);
+    expect(respondJSON.total).toBe(12);
+});
+
+test('contoh post', async ({ page }) => {
+    const apiContext = await request.newContext();
+    const postData = {
+      "name": "morpheus",
+      "job": "leader"
+    };
+    const res = await apiContext.post('https://reqres.in/api/users', { 
+        data : postData
+    });
+    expect(res.status()).toBe(201);
+    const respondJSON = await res.json();
+    expect(respondJSON.name).toBe('morpheus');
+    expect(respondJSON.job).toBe('leader');
 });
